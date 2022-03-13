7d208be594985b4f19db5b026a0ec0d63fc3032a
rkhademul8 <rkhademul8.com>
2022-03-13 12:11:56 +0600

    course_data_load_and_design_done

---
+++ src/components/Course/Course.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Course.css'
+const Course = (props) => {
+    console.log(props.course);
+
+    const {title,img,decription,price,duration}=props.course
+    return (
+        <div className='bg-slate-50 p-5 py-8 shadow-lg'>
+            <img className='image' src={img} alt="" />
+            <h1 className='text-xl font-black py-5 text-center'>{title}</h1>
+            <p className='text-base text-gray-500 text-justify'>{decription.slice(0,160)}</p>
+
+            <div className='flex justify-between py-8'>
+                <div><h1 className='text-lg font-black '>Duration:{duration}</h1></div>
+                <div><h1 className='text-lg font-black text-orange-600'>Price: {price} tk</h1></div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Course;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Course from '../Course/Course';
 
 const Home = () => {
+    const [cousers, setCourse]=useState([])
+
+    useEffect(()=>{
+        fetch('./api.JSON')
+        .then(res=>res.json())
+        .then(data=>setCourse(data))
+    },[])
     return (
 
         <div className='container mx-auto'>
             
-            {/* Feature part */}
+           {/* -----------------------------------
+                          Feature part start
+                          ------------------------------------*/}
             <div className='py-10'>
                 <h1 className='text-3xl text-center font-bold'>Awesome Feature</h1>
                 <p className='text-sm text-gray-400 text-center'>Replenish man have thing gathering lights yielding shall you</p>
@@ -27,10 +37,36 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-             {/* Feature part end*/}
+             {/* -----------------------------------
+                          Feature part end
+                          ------------------------------------*/}
 
-             
-           
+
+        {/* -----------------------------------
+                          course part start
+                          ------------------------------------*/}
+
+            <div>
+                <h1 className='text-3xl text-center font-bold'>Our Popular Courses</h1>
+                <p className='text-sm text-gray-400 text-center'>Replenish man have thing gathering lights yielding shall you</p>
+
+                <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4 w-4/5  mx-auto py-10">
+                
+                    {
+                        cousers.map(course=><Course key={course.id} course={course}>
+
+                        </Course>)
+                    }
+                    
+                </div>
+            </div>
+
+              {/* -----------------------------------
+                          course part end
+                          ------------------------------------*/}
+
+
+
         </div>
     );
 };
